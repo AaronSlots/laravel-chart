bc2a8bd690c58a55d6159adff41079856fdad685
AaronSlots <aaronslots@gmail.com>
2020-01-15 14:32:23 +0100

    up

---
+++ src/LaravelChartServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ASSoftware\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelChartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'ASSoftware/Laravel');
+        $this->publishes([
+            __DIR__.'/resources/templates' => resource_path('views/cms'),
+        ]);
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+}
